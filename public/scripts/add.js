06adbbacc209b2f0bb027afbada93ed4bcf3a27d
Matthew Yang <mwyang99@gmail.com>
2019-07-03 23:52:30 -0700

    post requests set up for adding restaurants

--- public/scripts/add.js
+++ public/scripts/add.js
@@ -1,22 +1,95 @@
+// Takes the string of a time in 24 hour time converts it into AMPM time, and then returns it.
+function convertTime(timeString) {
+    if (timeString == "") {
+        return "";
+    }
+
+    var hour = parseInt(timeString.substring(0, 2));
+    var minute = timeString.substring(3, 5);
+
+    if (hour <= 12) {
+        amPm = "am";
+        if (hour == 0) {
+            hour += 12;
+        }
+        convHour = hour.toString()
+    } 
+    
+    else {
+        amPm = "pm";
+        convHour = (hour - 12).toString()
+    }
+
+    return convHour + ":" + minute + amPm;
+}
+
+function getPrice(priceString) {
+    if (priceString == "$") {
+        return 1;
+    } else if (priceString == "$$") {
+        return 2;
+    } else {
+        return 3;
+    }
+}
+
 $(document).ready(function() {
     $('#add-form').submit(function() {
-        var name = $('#restaurantName').val();
-        var address = $('#restaurantAddress').val();
-        var description = $('#restaurantDesc').val();
+        var rName = $('#restaurantName').val();
+        var rAddress = $('#restaurantAddress').val();
+        var rDescription = $('#restaurantDesc').val();
+
         var priceRating = $('input[name="priceRadio"]:checked').val();
+        var numRating = getPrice(priceRating);
+
         var foodType = $('#foodTypes option:selected').val();
 
-        // Need better way to grab data for restaurant hours
         var arrLength = 7;
-        var timesFrom = new Array(arrLength);
-        var timesTo = new Array(arrLength);
-
+        var hours = new Array(arrLength);
         for (i = 0; i < arrLength; i++) {
             var fromId = '#timeFrom' + i.toString();
             var toId = '#timeTo' + i.toString();
+            var checkId = '#check' + i.toString();
 
-            timesFrom[i] = $(fromId).val();
-            timesTo[i] = $(toId).val();
+            initTimeFrom = $(fromId).val();
+            initTimeTo = $(toId).val();
+
+            fromTime = convertTime(initTimeFrom);
+            toTime = convertTime(initTimeTo);
+ 
+            if (fromTime != "" && toTime != "" && $(checkId).is(":checked")) {
+                restaurantHours = fromTime + "-" + toTime;
+            } else {
+                restaurantHours = "CLOSED";
+            }
+
+            hours[i] = restaurantHours;
         }
+
+        var restData = {
+            name: rName,
+            address: rAddress,
+            description: rDescription,
+            price: numRating,
+            sunday: hours[0],
+            monday: hours[1],
+            tuesday: hours[2],
+            wednesday: hours[3],
+            thursday: hours[4],
+            friday: hours[5],
+            saturday: hours[6]
+        }
+
+        $.ajax({
+            type: "post",
+            url: "/admin/add",
+            data: restData,
+            success: function() {
+                console.log("success");
+            },
+            error: function() {
+                console.log("failed");
+            }
+        })
     })
 })

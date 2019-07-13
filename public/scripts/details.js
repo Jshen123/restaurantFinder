d5a09e457832141dd35e687035089892e0e24f7e
Jshen123 <joeshen123@gmail.com>
2019-07-13 16:16:43 -0700

    Revert "Revert "Features/comments""

--- public/scripts/details.js
+++ public/scripts/details.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  const d = new Date();
   var rateStars = document.getElementsByClassName("rate-star");
   var rating = document.querySelector(".stars");
 
@@ -25,24 +26,30 @@ $(document).ready(function() {
     }
   }
 
+  // Run these when page loaded
   addClicks();
   setRating(rating.getAttribute("data-rating"));
+  
+  // function to get current date
+  function getDate() {
+    var months = ['January', "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    return `${months[d.getMonth()]} ${d.getDate()}, ${d.getFullYear()}`;
+  }
 
+  // Comment submithandler
   $('#comment-form').submit(function(e) {
     //prevent form submission
     e.preventDefault();
     e.stopPropagation();
 
-    var numStars = rating.getAttribute("data-rating");
-    var comment = $('#comment').val();
-    var id = $("#getId").attr("data-id");
+    var numStars = rating.getAttribute("data-rating");  //rating
+    var comment = $('#comment').val();                  //comment
+    var id = $("#getId").attr("data-id");               //restaurant_id
 
     var commentData = {
       rating: numStars,
       comment: comment,
-      username: "",
-      create_date: "",
-      restaurant_id: id
+      create_date: getDate()
     }
 
     var urlString = "/restaurants/" + id;
@@ -59,17 +66,19 @@ $(document).ready(function() {
 
   });
 
-  function postSuccessHandler(success) {
-    if (success) {
+  // Post request success handler
+  function postSuccessHandler(res) {
+    const {err, msg} = res;
+
+    if (err) {
+      alert(msg);   // Alert error message
+
+    } else {
       alert(`You've succesfully posted a review!`);
 
       //Clear comment
       $('#comment').val('');
-      
-    } else {
-      alert(`Failed to post a review!`);
     }
   };
 
-  
 });

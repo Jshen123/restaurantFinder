462f8d92a39b24a84ca2e832f6fb190b062ecf4b
Jared Ma <jaredma1234@gmail.com>
2019-07-14 02:10:38 -0700

    add all SFU restaurants

--- db/seeds/03_open_hours.js
+++ db/seeds/03_open_hours.js
@@ -193,6 +193,156 @@ exports.seed = function(knex, Promise) {
           thursday: '11:00am-8:00pm',
           friday: '11:00am-8:00pm',
           saturday: '11:00am-7:30pm'
+        },
+        {
+          restaurant_id: 20,
+          sunday: 'CLOSED',
+          monday: '7:30am-8:00pm',
+          tuesday: '7:30am-8:00pm',
+          wednesday: '7:30am-8:00pm',
+          thursday: '7:30am-8:00pm',
+          friday: '7:30am-8:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 21,
+          sunday: 'CLOSED',
+          monday: '6:00am-7:30pm',
+          tuesday: '6:00am-7:30pm',
+          wednesday: '6:00am-7:30pm',
+          thursday: '6:00am-7:30pm',
+          friday: '6:00am-7:30pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 22,
+          sunday: 'CLOSED',
+          monday: '11:00am-6:00pm',
+          tuesday: '11:00am-6:00pm',
+          wednesday: '11:00am-6:00pm',
+          thursday: '11:00am-7:00pm',
+          friday: '11:00am-4:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 23,
+          sunday: 'CLOSED',
+          monday: '7:00am-7:00pm',
+          tuesday: '7:00am-7:00pm',
+          wednesday: '7:00am-7:00pm',
+          thursday: '7:00am-7:00pm',
+          friday: '7:00am-7:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 24,
+          sunday: 'CLOSED',
+          monday: '11:00am-7:00pm',
+          tuesday: '11:00am-7:00pm',
+          wednesday: '11:00am-7:00pm',
+          thursday: '11:00am-7:00pm',
+          friday: '11:00am-4:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 25,
+          sunday: 'CLOSED',
+          monday: '11:00am-6:00pm',
+          tuesday: '11:00am-6:00pm',
+          wednesday: '11:00am-6:00pm',
+          thursday: '11:00am-6:00pm',
+          friday: '11:00am-5:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 26,
+          sunday: 'CLOSED',
+          monday: '10:15am-6:00pm',
+          tuesday: '10:15am-6:00pm',
+          wednesday: '10:15am-6:00pm',
+          thursday: '10:15am-6:00pm',
+          friday: '10:15am-5:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 27,
+          sunday: 'CLOSED',
+          monday: '10:30am-5:00pm',
+          tuesday: '10:30am-5:00pm',
+          wednesday: '10:30am-5:00pm',
+          thursday: '10:30am-5:00pm',
+          friday: '10:30am-4:00pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 28,
+          sunday: '11:00am-12:00am',
+          monday: '11:00am-12:00am',
+          tuesday: '11:00am-12:00am',
+          wednesday: '11:00am-12:00am',
+          thursday: '11:00am-1:00am',
+          friday: '11:00am-1:00am',
+          saturday: '11:00am-1:00am'
+        },
+        {
+          restaurant_id: 29,
+          sunday: 'CLOSED',
+          monday: '8:00am-7:30pm',
+          tuesday: '8:00am-7:30pm',
+          wednesday: '8:00am-7:30pm',
+          thursday: '8:00am-7:30pm',
+          friday: '8:00am-7:30pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 30,
+          sunday: '8:00am-10:00pm',
+          monday: '7:00am-10:00pm',
+          tuesday: '7:00am-10:00pm',
+          wednesday: '7:00am-10:00pm',
+          thursday: '7:00am-12:00am',
+          friday: '7:00am-12:00am',
+          saturday: '8:00am-12:00am'
+        },
+        {
+          restaurant_id: 31,
+          sunday: '8:00am-4:00pm',
+          monday: '8:00am-4:00pm',
+          tuesday: '8:00am-4:00pm',
+          wednesday: '8:00am-4:00pm',
+          thursday: '8:00am-4:00pm',
+          friday: '8:00am-4:00pm',
+          saturday: '8:00am-4:00pm'
+        },
+        {
+          restaurant_id: 32,
+          sunday: '7:00am-10:00pm',
+          monday: '7:00am-10:00pm',
+          tuesday: '7:00am-10:00pm',
+          wednesday: '7:00am-10:00pm',
+          thursday: '7:00am-10:00pm',
+          friday: '7:00am-10:00pm',
+          saturday: '7:00am-10:00pm'
+        },
+        {
+          restaurant_id: 33,
+          sunday: 'CLOSED',
+          monday: '6:00am-7:30pm',
+          tuesday: '6:00am-7:30pm',
+          wednesday: '6:00am-7:30pm',
+          thursday: '6:00am-7:30pm',
+          friday: '6:00am-7:30pm',
+          saturday: 'CLOSED'
+        },
+        {
+          restaurant_id: 34,
+          sunday: 'CLOSED',
+          monday: '8:00am-7:00pm',
+          tuesday: '8:00am-7:00pm',
+          wednesday: '8:00am-7:00pm',
+          thursday: '8:00am-7:00pm',
+          friday: '8:00am-7:00pm',
+          saturday: 'CLOSED'
         }
       ]);
     });

247f77058a6dcf402af4ea5105ed6feb22653bd5
leungmanchiu <leungmanchiu123@gmail.com>
2019-07-22 01:25:10 -0700

    get tags from query string and filter to render page

--- routes/routes.js
+++ routes/routes.js
@@ -281,39 +281,54 @@ module.exports = function (queries, io) {
   router.get('/restaurants', (req, res) => {
     const today = moment().tz("America/Vancouver")
     const weekdays = ['sunday','monday','tuesday','wednesday','thursday','friday','saturday']
-    // determine which day of the week
-    const day = weekdays[today.day()]
-    // console.log(today)
+    const day = weekdays[today.day()]   // determine which day of the week
 
-    queries.getRestaurants((value) => {
+    const tags = req.query.tag;
 
+    if (!tags) {
+      // default get '/restaurants' with no query string
+      queries.getRestaurants((value) => {
+        const payload = get_payload(value);
+        return res.render('pages/restaurants', payload);
+      });
+
+    } else {
+      // filter restaurants with tags
+      //queries.filterRestaurants(tags, (value) => {
+      queries.getRestaurants((value) => {
+        const payload = get_payload(value);
+        return res.render('pages/restaurants', payload);
+      });
+    }
+
+    // function to get the payload for rendering the page
+    function get_payload(value) {
       const payload = {
-                        user_id: req.session.user_id, 
-                        open:[],
-                        closed:[]
-                      };
+        user_id: req.session.user_id, 
+        open:[],
+        closed:[]
+      };
         
-        value.forEach(function(val){
+      value.forEach(function(val) {
+        if (val[day] == 'CLOSED'){
+          payload.closed.push(val)
+        } else if (val[day] == 'OPEN') {
+          payload.open.push(val)
+        } else {
+          const businessHours = val[day].split("-")
+          const startHour = moment(businessHours[0], "LT").tz("America/Vancouver")
+          const endHour = moment(businessHours[1], "LT").tz("America/Vancouver")            
 
-
-          if (val[day] == 'CLOSED'){
-            payload.closed.push(val)
-          } else if (val[day] == 'OPEN') {
+          if (today.isBetween(startHour, endHour) || today.isSame(startHour) || today.isSame(endHour)){
             payload.open.push(val)
           } else {
-            const businessHours = val[day].split("-")
-            const startHour = moment(businessHours[0], "LT").tz("America/Vancouver")
-            const endHour = moment(businessHours[1], "LT").tz("America/Vancouver")            
-
-            if (today.isBetween(startHour, endHour) || today.isSame(startHour) || today.isSame(endHour)){
-              payload.open.push(val)
-            } else {
-              payload.closed.push(val)
-            }
+            payload.closed.push(val)
           }
-        })
-        res.render('pages/restaurants', payload)
-    })
+        }
+      })
+
+      return payload;
+    }
     
   })
 

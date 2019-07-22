60073e2eee79306d6f9d9bfb8678e55b61fa9236
Matthew Yang <mwyang99@gmail.com>
2019-07-22 01:18:28 -0700

    add tag checkboxes and styling

--- public/scripts/restaurants.js
+++ public/scripts/restaurants.js
@@ -1,6 +1,21 @@
 $(document).ready(function() {
-    $('.card-title.link').click(function() {
-        var restId = $(this).data('id');
-        console.log(restId);
+    $('#test').submit(function() {
+        var qString = "?"
+
+        var arrLength = 27;
+        for (i = 0; i < arrLength; i++) {
+            var checkId = '#check' + i;
+            var checkBox = $(checkId)
+            if (checkBox.is(':checked')) {
+                qString += "tag=" + checkBox.val() + "&";
+            }
+        }
+
+        // Check if last character is &
+        if (qString[qString.length - 1] == '&') {
+            qString = qString.substring(0, qString.length - 1);
+        }
+
+        console.log(qString);
     })
 })
